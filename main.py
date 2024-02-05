c35e847d4b0b91d5bbd05d9a350fe99c56209cce
ingrid <kamilaingridc@gmail.com>
2024-02-05 16:31:03 -0300

    initial classes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI  # importa biblioteca
+# importa bibliotecas
+from fastapi import FastAPI  
+from model import Pokemons
+from fastapi import HTTPException
 
 app = FastAPI()  # instancia a biblioteca
 
@@ -26,9 +29,20 @@ async def get_pokemons(): # retorna todos os pokemons
 
 @app.get('/pokemon/{pokemon_id}')
 async def get_pokemon(pokemon_id: int):
-    pokemon = pokemons[pokemon_id]
-    return pokemon
+    if pokemon_id not in pokemons:  # return a message for false id 
+        raise HTTPException (status_code=404, detail="Pokemon não encontrado.")
+    return pokemons[pokemon_id]
 
+# POST 
+@app.post('/pokemon')
+async def post_pokemon(pokemon: Pokemons):
+    if pokemon.id not in pokemons:
+        pokemons[pokemon.id] = pokemon
+        return pokemon
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Já existe um pokemon com esse id.")
+
+# roda o servidor
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port = 8000, log_level = "info", reload=True)
